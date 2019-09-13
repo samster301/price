6477b02d61a759b7b150b71755f43ecd1fb0c3db
Samaresh Mohanan <sam.i.am.06@gmail.com>
2019-09-12 23:14:46 -0500

    Externilized configuration

--- src/main/java/com/wipro/price/controller/PriceController.java
+++ src/main/java/com/wipro/price/controller/PriceController.java
@@ -7,13 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.math.BigDecimal;
 import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/price")
